c455fe611b5e7b4a4ac7a5820650bcc0bc934086
MrRidd4k3 <ayinla_ridwan@yahoo.com>
2023-03-07 18:48:56 +0000

    revised work on the file

--- userMgt.java
+++ userMgt.java
@@ -1,8 +1,6 @@
-Ridwan 
+Ridwan
 Opeyemi
 Ayinla
-Nuhu
-this in a training for git and github
-God help my soul
-I pray this works out as i planned
-I love u damilola
+Interested in coding
+Devops precisely
+God help me
